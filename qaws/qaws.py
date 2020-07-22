e05aca8bc16edbbfd958c89e6963f59b559ce553
bqbn <bqbn@openken.com>
2020-07-22 00:03:19 -0700

    Apply black formatting

--- qaws/qaws.py
+++ qaws/qaws.py
@@ -8,50 +8,51 @@ from .ec2 import EC2
 # without any command.
 @click.group(invoke_without_command=True)
 @click.option(
-    '--all-commands', is_flag=True, help='List all known commands.',
+    "--all-commands", is_flag=True, help="List all known commands.",
 )
 def cli(*args, **kwargs):
-    if kwargs['all_commands']:
-        for cmd in AwsCompleter().complete('aws', 3):
+    if kwargs["all_commands"]:
+        for cmd in AwsCompleter().complete("aws", 3):
             click.echo(cmd)
 
 
 @cli.command()
 @click.option(
-    '--attrib', '-a',
+    "--attrib",
+    "-a",
     default=[
-        'InstanceId',
-        'InstanceType',
-        'KeyName',
-        'ImageId',
-        'PrivateIpAddress',
-        'LaunchTime',
+        "InstanceId",
+        "InstanceType",
+        "KeyName",
+        "ImageId",
+        "PrivateIpAddress",
+        "LaunchTime",
     ],
     show_default=True,
     multiple=True,
-    help='One or multiple comma-seperated attributes to show',
+    help="One or multiple comma-seperated attributes to show",
 )
 @click.option(
-    '--limit',
+    "--limit",
     default=0,
     show_default=True,
-    help='Limit the number of results that get shown. 0 means no limit.',
+    help="Limit the number of results that get shown. 0 means no limit.",
 )
 @click.option(
-    '--known-attributes', is_flag=True, help='List all known attributes.',
+    "--known-attributes", is_flag=True, help="List all known attributes.",
 )
 @click.option(
-    '--output',
-    type=click.Choice(['json', 'table', 'text'], case_sensitive=False),
-    default='text',
+    "--output",
+    type=click.Choice(["json", "table", "text"], case_sensitive=False),
+    default="text",
     show_default=True,
-    help='The formatting style for command output.',
+    help="The formatting style for command output.",
 )
-@click.option('--tag-key', '-k', default='Name', show_default=True)
-@click.option('--tag-value', '-v')
+@click.option("--tag-key", "-k", default="Name", show_default=True)
+@click.option("--tag-value", "-v")
 def ec2(*args, **kwargs):
     EC2(*args, **kwargs).run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
