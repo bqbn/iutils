e8a60d729c042aa0cacc7f8361c7aa50389d894d
bqbn <bqbn@openken.com>
2020-07-25 01:18:47 -0700

    Update the elb command and add the resouces command

--- qaws/elb.py
+++ qaws/elb.py
@@ -8,26 +8,39 @@ class ELB(Resource):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.show_limits = kwargs["show_limits"]
+        self._list = kwargs["list"]
+        self.tag_key = kwargs.get("tag_key")
+        self.tag_value = kwargs.get("tag_value")
+
+        self.client = boto3.client("elb")
+        self.tagging_api_client = boto3.client("resourcegroupstaggingapi")
 
     def run(self):
         if self.show_limits:
-            limits = []
+            self.show_elb_limits()
+            return
+        if self._list:
+            self.list_elbs()
 
-            client = boto3.client("elb")
-            paginator = client.get_paginator("describe_account_limits")
-            pagination_config = {
-                "MaxItems": 100,
-                "PageSize": 100,  # PageSize should never be greater than MaxItems.
-                "StartingToken": "",
-            }
+    def show_elb_limits(self):
+        limits = []
 
-            while True:
-                for res in paginator.paginate(PaginationConfig=pagination_config):
-                    limits += res["Limits"]
-                if res.get("NextMarker"):
-                    pagination_config["StartingToken"] = res["NextMarker"]
-                else:
-                    break
+        paginator = self.client.get_paginator("describe_account_limits")
+        pagination_config = {
+            "MaxItems": 100,
+            "PageSize": 100,  # PageSize should never be greater than MaxItems.
+        }
 
-            for item in limits:
-                click.echo(f"{item['Name']}: {item['Max']} max")
+        while True:
+            for res in paginator.paginate(PaginationConfig=pagination_config):
+                limits += res["Limits"]
+            if res.get("NextMarker"):
+                pagination_config["StartingToken"] = res["NextMarker"]
+            else:
+                break
+
+        for item in limits:
+            click.echo(f"{item['Name']}: {item['Max']} max")
+
+    def list_elbs(self):
+        print(f'Use the "qaws resources --types elasticloadbalancing" command instead.')
--- qaws/qaws.py
+++ qaws/qaws.py
@@ -3,6 +3,7 @@ import click
 from awscli.completer import Completer as AwsCompleter
 from .ec2 import EC2
 from .elb import ELB
+from .resources import Resources
 
 
 # Need invoke_without_commands=True because need to call --all-commands
@@ -62,13 +63,41 @@ def ec2(*args, **kwargs):
     "--limit",
     default=0,
     show_default=True,
-    help="Limit the number of results that get shown. 0 means no limit.",
+    help="Limit the number of results that get shown. 0 means no limit",
+)
+@click.option(
+    "--list", "-l", is_flag=True, help="List ELBs for this account",
 )
 @click.option(
     "--show-limits", is_flag=True, help="Show ELB limits for this account.",
 )
+@click.option("--tag-key", "-k")
+@click.option("--tag-value", "-v")
 def elb(*args, **kwargs):
     ELB(*args, **kwargs).run()
+
+
+@cli.command()
+@click.option(
+    "--types",
+    default=[],
+    multiple=True,
+    help=(
+        'Filter resources by types. Types can be in the form of "service" or '
+        '"service:resouce_type", for example, "ec2" or "ec2:instance".'
+    ),
+)
+@click.option(
+    "--with-tags",
+    help='Filter resources by specified tags. Tags can be in the form of "key:value", "key:value1,value2,..." or just "key:".',
+    multiple=True,
+    default=[],
+)
+@click.option(
+    "--count-by-service", is_flag=True, help="Count resources by service name.",
+)
+def resources(*args, **kwargs):
+    Resources(*args, **kwargs).run()
 
 
 if __name__ == "__main__":
