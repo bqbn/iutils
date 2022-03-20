bf6ac47d1aa22203f4b3189c6aac6992fa9dc806
bqbn <bqbn@openken.com>
2022-03-19 17:54:53 -0700

    Fix an installation time dependency error (missing requests)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="iutils",
-    version="0.0.15",
+    version="0.0.17",
     description="""A collection of my utility modules""",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -31,12 +31,25 @@ setup(
     keywords="devops utility utils",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
     python_requires=">=3, <4",
-    install_requires=["boto3>=1.14.33", "click>=7.1.2",],
+    install_requires=[
+        "awscli==1.22.77",
+        "boto3==1.21.22",
+        "click==8.0.4",
+        "jmespath==1.0.0",
+        "requests==2.27.1",
+    ],
     # Additional groups of dependencies. They can be install by using
     # the "extras" syntax, for example:
     #
     #   $ pip install <module-name>[dev]
-    extras_require={"dev": ["twine>=3.2.0", "wheel>=0.34.2",], "test": [],},
+    extras_require={
+        "dev": [
+            "pipenv-setup~=3.2",
+            "twine~=3.2",
+            "wheel~=0.34",
+        ],
+        "test": [],
+    },
     project_urls={
         "Bug Reports": "https://github.com/bqbn/iutils/issues",
         "Source": "https://github.com/bqbn/iutils",
@@ -44,9 +57,9 @@ setup(
     # Needed by the pipenv-setup tool.
     dependency_links=[],
     entry_points={
-        'console_scripts': [
-            'qaws=qaws.qaws:cli',
-            'qsentry=qsentry.qsentry:main',
+        "console_scripts": [
+            "qaws=qaws.qaws:cli",
+            "qsentry=qsentry.qsentry:main",
         ],
     },
 )
