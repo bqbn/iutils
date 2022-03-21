6ee44477c3cd5bb4091ce027b62054ca0d22cab8
bqbn <bqbn@openken.com>
2022-03-20 17:14:38 -0700

    Change list-admins to a more general command

--- src/qsentry/qsentry.py
+++ src/qsentry/qsentry.py
@@ -8,17 +8,19 @@ common_options = [
     click.option(
         "--auth-token",
         envvar="QSENTRY_AUTH_TOKEN",
-        help="The auth token for invoke sentry apis.",
+        help="The auth token for invoke sentry apis. Can read from the QSENTRY_AUTH_TOKEN env variable.",
     ),
     click.option(
         "--host-url",
-        default="https://sentry.io",
-        help="The host URL for the sentry service.",
+        envvar="QSENTRY_HOST_URL",
+        default="https://sentry.io/",
+        show_default=True,
+        help="The host URL for the sentry service. Can read from the QSENTRY_HOST_URL env variable.",
     ),
     click.option(
         "--org",
         envvar="QSENTRY_ORG_SLUG",
-        help="The organization slug.",
+        help="The organization slug. Can read from the QSENTRY_ORG_SLUG env variable.",
     ),
 ]
 
@@ -44,13 +46,19 @@ def main(*args, **kwargs):
     envvar="QSENTRY_TEAM_SLUG",
     help="The team slug.",
 )
-def list_admins(**kwargs):
+@click.option(
+    "--role",
+    default="admin",
+    show_default=True,
+    help="The role of the member."
+)
+def members(**kwargs):
     for member in SentryApi(
         kwargs["host_url"], kwargs["auth_token"]
     ).filter_by_role_name(
         org_slug=kwargs["org"],
         team_slug=kwargs["team"],
-        role_name="admin",
+        role_name=kwargs["role"],
         attributes=["id", "name", "email"],
     ):
         print(f"{member['id']}, {member['name']}, {member['email']}")
